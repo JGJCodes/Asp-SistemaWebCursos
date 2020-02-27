761c3eee03c51325e55654578f6576ef8361f172
Jorge Guzmán Juárez <54335344+JGJCodes@users.noreply.github.com>
2020-02-26 21:52:49 -0600

    model class categoria

---
+++ SistemaWebCursos/SistemaWebCursos/Data/SistemaWebCursosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebCursos.Models;
+
+namespace SistemaWebCursos.Data
+{
+    public class SistemaWebCursosContext : DbContext
+    {
+        public SistemaWebCursosContext (DbContextOptions<SistemaWebCursosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaWebCursos.Models.Categoria> Categoria { get; set; }
+    }
+}
