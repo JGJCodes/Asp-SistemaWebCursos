78e63cbcbf13ad6e3823a62b387406e11659a021
Jorge Guzmán Juárez <54335344+JGJCodes@users.noreply.github.com>
2020-02-28 21:48:58 -0600

    Create views of Categoria

---
+++ SistemaWebCursos/Readme.txt
@@ -0,0 +1,12 @@
+Carpeta contenedora de los archivos del proyecto
+
+*bin 	     => Archivos de paquetes de C#
+*Controllers => Controladores creador por Entity Framework
+*Data 	     => Archivos referentes a la BD
+*Models      => Clases de los objetos de la BD
+*obj	     => Archivos de depuracion y paquetes nuget
+*Views 	     => Paginas web aspx del sistema
+*wwwroot     => Hojas de estilos css, Imagenes png, Funciones js, Librerias
+
++appsetting.json => Archivo contenedor de los datos de la conexion con la BD
++Startup.cs 	 => Archivo que define los metodos de configuracion de inicio 
--- SistemaWebCursos/SistemaWebCursos/Data/DbInitializer.cs
+++ SistemaWebCursos/SistemaWebCursos/Data/DbInitializer.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SistemaWebCursos.Models;
 
 namespace SistemaWebCursos.Data
 {
     public class DbInitializer
     {
+        /**
+         * Metodo inicializador de la Base de Datos
+         * Recibe un parametro contexto vacio y le 
+         * asigna los valores de la BD
+         * **/
+        public static void Initialize(SistemaWebCursosContext context)
+        {
+            context.Database.EnsureCreated();//Crea la Base de Datos
+
+            //Buscar si existen registros en la tabla categoria
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[] //Crea un arreglo con los registros de la tabla Categoria
+            {
+                new Categoria{Nombre="Programación",Descripcion="Cursos de programación",Estado=true },
+                new Categoria{Nombre="Diseño grafico",Descripcion="Cursos de diseño grafico",Estado=true },
+            };
+
+            foreach (Categoria c in categorias) //Asigna los valores del arreglo a la base de datos
+            {
+                context.Categoria.Add(c);
+            }
+            context.SaveChanges();//Guarda los cambios realizados
+
+        }
     }
 }
